bce4a25866897ca52e6f167975bed06390c1b44e
taeho <tae_ho_@naver.com>
2025-05-18 11:24:16 +0900

    feat: map page

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,25 @@
-import { Route, Routes } from 'react-router';
-import './App.css';
-import { Header } from './layouts/Header';
-import { SNB } from './layouts/SNB';
-import { routes } from './routes';
+import { Route, Routes } from "react-router";
+import "./App.css";
+import { Header } from "./layouts/Header";
+import { SNB } from "./layouts/SNB";
+import { routes } from "./routes";
+import { Toaster } from "@/components/ui/sonner";
 
 function App() {
-	return (
-		<div className='relative h-full w-full flex flex-col'>
-			<Header />
-			<Routes>
-				{routes.map((route, index) => {
-					return (
-						<Route
-							key={index}
-							path={route.path}
-							element={route.element}
-						/>
-					);
-				})}
-			</Routes>
-			<SNB />
-		</div>
-	);
+  return (
+    <div className="relative h-full w-full flex flex-col">
+      <Header />
+      <Routes>
+        {routes.map((route, index) => {
+          return (
+            <Route key={index} path={route.path} element={route.element} />
+          );
+        })}
+      </Routes>
+      <SNB />
+      <Toaster richColors />
+    </div>
+  );
 }
 
 export default App;
--- src/pages/MapPage/index.tsx
+++ src/pages/MapPage/index.tsx
@@ -1,10 +1,69 @@
+import Typography from "@/components/ui/typography";
+import MapImage from "@/assets/images/map.webp";
+import { toast } from "sonner";
+
+const address = [
+  {
+    type: "지번",
+    address: "서울 강남구 역삼로 607",
+  },
+  {
+    type: "도로명",
+    address: "서울 강남구 대치동 1004-3",
+  },
+];
+
 export default function MapPage() {
-	return (
-		<div className='flex flex-col w-full h-full bg-white'>
-			<div className='flex flex-col w-full h-full p-4'>
-				<h1 className='text-2xl font-bold'>Map Page</h1>
-				<p className='text-gray-600'>This is the map page content.</p>
-			</div>
-		</div>
-	);
+  return (
+    <main className="flex flex-col w-full h-full min-h-[100vh] flex-1 text-left">
+      <div
+        onClick={() =>
+          window.open(
+            "https://map.naver.com/p/entry/place/36462628?c=15.52,0,0,0,dh"
+          )
+        }
+      >
+        <img
+          src={MapImage}
+          alt="그랜드힐 컨벤션 오시는길"
+          className="w-full aspect-square"
+        />
+      </div>
+      <div className="my-1 mr-2 ml-[auto]">
+        <Typography variant="caption1">
+          * 사진을 클릭하시면 네이버 지도로 이동합니다.
+        </Typography>
+      </div>
+      <div className="flex flex-col my-5 pl-3">
+        <Typography variant="title2" className="mb-3">
+          그랜드힐 컨벤션
+        </Typography>
+
+        {address.map((item, index) => (
+          <div key={index}>
+            <Typography variant="caption1" className="mr-2 inline-block w-9">
+              {item.type}
+            </Typography>
+            <Typography variant="body1">{item.address}</Typography>
+            <Typography
+              variant="body2"
+              className="ml-1 text-[var(--focused)]"
+              onClick={() => {
+                navigator.clipboard.writeText(item.address);
+                toast.success(
+                  <div className="text-start">
+                    {item.address}
+                    <br />
+                    주소를 복사했습니다.
+                  </div>
+                );
+              }}
+            >
+              복사
+            </Typography>
+          </div>
+        ))}
+      </div>
+    </main>
+  );
 }
