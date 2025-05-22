3aaf4845a5b3adbbe09246422a88b0723cbea5cb
taehoinnodep <147114106+taehoinnodep@users.noreply.github.com>
2025-05-22 18:00:57 +0900

    feat: profile page design

--- src/components/ui/typography.tsx
+++ src/components/ui/typography.tsx
@@ -1,91 +1,91 @@
-import { cn } from "@/lib/utils";
-import type { CSSProperties, ReactNode } from "react";
+import { cn } from '@/lib/utils';
+import type { CSSProperties, ReactNode } from 'react';
 
 interface TypographyProps {
-  variant:
-    | "display1"
-    | "display2"
-    | "title2"
-    | "title3"
-    | "headline1"
-    | "label1"
-    | "label2"
-    | "heading1"
-    | "heading2"
-    | "caption1"
-    | "caption2"
-    | "body1"
-    | "body2";
-  children: ReactNode;
-  className?: string;
-  style?: CSSProperties;
-  title?: string;
+	variant?:
+		| 'display1'
+		| 'display2'
+		| 'title2'
+		| 'title3'
+		| 'headline1'
+		| 'label1'
+		| 'label2'
+		| 'heading1'
+		| 'heading2'
+		| 'caption1'
+		| 'caption2'
+		| 'body1'
+		| 'body2';
+	children: ReactNode;
+	className?: string;
+	style?: CSSProperties;
+	title?: string;
 }
 
 const Typography = ({
-  variant,
-  children,
-  className,
-  style,
-  title,
-  ...props
-}: TypographyProps & React.ComponentProps<"span">) => {
-  let computedClassName = "";
+	variant = 'label1',
+	children,
+	className,
+	style,
+	title,
+	...props
+}: TypographyProps & React.ComponentProps<'span'>) => {
+	let computedClassName = '';
 
-  switch (variant) {
-    case "display1":
-      computedClassName = "text-[3.5rem] font-bold";
-      break;
-    case "display2":
-      computedClassName = "text-[2.5rem] font-bold";
-      break;
-    case "title2":
-      computedClassName = "text-[1.75rem] font-semibold";
-      break;
-    case "title3":
-      computedClassName = "text-[1.25rem] font-semibold";
-      break;
-    case "headline1":
-      computedClassName = "text-[1.125rem] font-bold";
-      break;
-    case "label1":
-      computedClassName = "text-[0.875rem] font-normal ";
-      break;
-    case "label2":
-      computedClassName = "text-[0.875rem] font-normal ";
-      break;
-    case "heading1":
-      computedClassName = "text-[1.375rem] font-bold";
-      break;
-    case "heading2":
-      computedClassName = "text-[1.25rem] font-bold";
-      break;
-    case "caption1":
-      computedClassName = "text-[0.75rem] font-normal text-[var(--shy)]";
-      break;
-    case "caption2":
-      computedClassName = "text-[0.6875rem] font-normal text-[var(--shy)]";
-      break;
-    case "body1":
-      computedClassName = "text-[1rem] font-medium";
-      break;
-    case "body2":
-      computedClassName = "text-[0.9375rem] font-light";
-      break;
-    default:
-      break;
-  }
+	switch (variant) {
+		case 'display1':
+			computedClassName = 'text-[3.5rem] font-bold';
+			break;
+		case 'display2':
+			computedClassName = 'text-[2.5rem] font-bold';
+			break;
+		case 'title2':
+			computedClassName = 'text-[1.75rem] font-semibold';
+			break;
+		case 'title3':
+			computedClassName = 'text-[1.25rem] font-semibold';
+			break;
+		case 'headline1':
+			computedClassName = 'text-[1.125rem] font-bold';
+			break;
+		case 'label1':
+			computedClassName = 'text-[0.875rem] font-normal ';
+			break;
+		case 'label2':
+			computedClassName = 'text-[0.875rem] font-normal ';
+			break;
+		case 'heading1':
+			computedClassName = 'text-[1.375rem] font-bold';
+			break;
+		case 'heading2':
+			computedClassName = 'text-[1.25rem] font-bold';
+			break;
+		case 'caption1':
+			computedClassName = 'text-[0.75rem] font-normal text-[var(--shy)]';
+			break;
+		case 'caption2':
+			computedClassName = 'text-[0.6875rem] font-normal text-[var(--shy)]';
+			break;
+		case 'body1':
+			computedClassName = 'text-[1rem] font-medium';
+			break;
+		case 'body2':
+			computedClassName = 'text-[0.9375rem] font-light';
+			break;
+		default:
+			break;
+	}
 
-  return (
-    <span
-      className={cn(computedClassName, className)}
-      style={style}
-      title={title}
-      {...props}
-    >
-      {children}
-    </span>
-  );
+	return (
+		<span
+			className={cn(computedClassName, className)}
+			style={style}
+			title={title}
+			{...props}
+		>
+			{children}
+		</span>
+	);
 };
 
 export default Typography;
--- src/pages/ProfilePage/index.tsx
+++ src/pages/ProfilePage/index.tsx
@@ -1,21 +1,76 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Button } from '@/components/ui/button';
+import Typography from '@/components/ui/typography';
+import profiles from '@/data/profile.json';
+import { useEffect, useState } from 'react';
 
 interface Props {
-  userID: string;
+	userID: string;
 }
 
+const NumberPannel = (number: number, label: string) => {
+	return (
+		<div className='flex flex-col w-full items-start'>
+			<Typography>{number}</Typography>
+			<Typography className='font-regular'>{label}</Typography>
+		</div>
+	);
+};
+
 export default function ProfilePage({ userID }: Props) {
-  return (
-    <div className="flex flex-col w-full h-full">
-      <span>{userID}</span>
-      <div className="flex flex-row gap-4 w-full mt-5">
-        <Avatar className="w-[77px] h-[77px]">
-          <AvatarImage src="" />
-          <AvatarFallback className="capitalize">
-            {userID.substring(0, 2)}
-          </AvatarFallback>
-        </Avatar>
-      </div>
-    </div>
-  );
+	const [user, setUser] = useState<any>(null);
+
+	useEffect(() => {
+		const fetchUser = async () => {
+			const userData = profiles.find((profile) => profile.id === userID);
+			if (userData) {
+				setUser(userData);
+			}
+		};
+
+		fetchUser();
+	}, [userID]);
+
+	return (
+		<div className='flex flex-col w-full h-full pt-3'>
+			<div
+				id='profile-area'
+				className='flex flex-col w-full h-full items-start px-5'
+			>
+				<Typography variant='headline1'>{userID}</Typography>
+				<div className='flex flex-row gap-4 w-full mt-5'>
+					<Avatar className='w-[77px] h-[77px]'>
+						<AvatarImage src={user?.image} />
+						<AvatarFallback className='capitalize'>{userID.substring(0, 2)}</AvatarFallback>
+					</Avatar>
+					<div className='flex flex-col w-full items-start'>
+						<Typography>{user?.name}</Typography>
+						<div className='flex flex-row w-full mt-2'>
+							{NumberPannel(0, 'Posts')}
+							{NumberPannel(0, 'Followers')}
+							{NumberPannel(0, 'Following')}
+						</div>
+					</div>
+				</div>
+				<Typography className='mt-2 text-[var(--ring)]'>{user?.job}</Typography>
+				<Typography>{user?.comment}</Typography>
+				<div className='w-full flex flex-row gap-2 my-2 items-center justify-center'>
+					<Button
+						variant='outline'
+						className='border-white flex-1'
+						size='sm'
+					>
+						축의 보내기
+					</Button>
+					<Button
+						variant='outline'
+						className='border-white flex-1'
+						size='sm'
+					>
+						메시지 보내기
+					</Button>
+				</div>
+			</div>
+		</div>
+	);
 }
